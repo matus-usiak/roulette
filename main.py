360da8a0ff0b9893cd4089db4223a82e4568588e
Matúš Ušiak <43740719+matus-usiak@users.noreply.github.com>
2022-10-19 16:17:01 +0200

    fully functional

--- main.py
+++ main.py
@@ -1,13 +1,15 @@
 import random
 
-credit = int(input("Please enter your credit "))
+credit = int(input("Please enter your credit: "))
+
+if credit < 0:
+    print("Invalid input! Please enter a number greater than 0.")
 
 while credit > 0:
     bet = input(
-        "Place your bets in form of bet type and amount.\n"
-        "Bet types are: single number, red, black, odd, even, 1to18, 19to36 followed by space separated by "
-        "semicolon.\n"
-        "For example: 18 20;odd 10; black 30\n"
+        "Place your bets in form of <bet type><space><amount> separated by semicolon.\n"
+        "Bet types are: single number, red, black, odd, even, 1to18, 19to36. \n"
+        "For example: 18 20;odd 10; black 30\n\n"
         "Your bet: ")
 
     betlist = bet.split(";")
@@ -18,17 +20,22 @@ while credit > 0:
         bettypelist.append(bt.split(" ")[0])
         betamountlist.append(bt.split(" ")[1])
 
-    spinresult = int(random.randint(0, 36))
+    totalbet = sum(map(int, betamountlist))
 
-    print("Spin result: ", spinresult)
+    if totalbet > credit:
+        print("Not enough credit!")
+
+    spinresult = int(random.randint(0, 36))
 
     redspaces = [1, 3, 5, 7, 9, 12, 14, 16, 18, 19, 21, 23, 25, 27, 30, 32, 34, 36]
 
     spinresultlist = [str(spinresult)]
 
+    print("Spin result: ", spinresult, " - ", spinresultlist)
+
     if spinresult in redspaces:
         spinresultlist.append("red")
-    if spinresult % 2 ==0:
+    if spinresult % 2 == 0:
         spinresultlist.append("even")
     if spinresult not in redspaces:
         spinresultlist.append("black")
@@ -43,19 +50,20 @@ while credit > 0:
 
     i = 0
     while i < len(betlist):
-
         if bettypelist[i] in spinresultlist:
             winningamount = winningamount + int(betamountlist[i]) + (int(betamountlist[i]) * 2)
 
-        if spinresultlist[0] in betlist[i].split(" ")[0]:  # WORKS
+        if spinresultlist[0] in betlist[i].split(" ")[0]:
             winningamount = winningamount + int(betamountlist[i]) + (int(betamountlist[i]) * 35)
 
-        if bettypelist[i] not in spinresultlist:  # WORKS
+        if bettypelist[i] not in spinresultlist:
             winningamount = winningamount - int(betamountlist[i])
 
         i = i + 1
-    print("Total winning: ", winningamount)
     credit = credit + winningamount
+    print("Spin result: ", spinresult, " - ", spinresultlist)
+    print("Bet amount: ", totalbet)
+    print("Return: ", winningamount)
     print("Remaining credit: ", credit)
 else:
     print("No credit")
